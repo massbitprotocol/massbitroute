ab1d12a21be6cd4235aea891d3629d2041373af6
Vu Tran <baysao@gmail.com>
2021-10-14 15:17:32 +0700

    api: update stats

--- app/src/sites/services/gateway/config.lua
+++ app/src/sites/services/gateway/config.lua
@@ -9,6 +9,21 @@ local _config = {
     templates = {},
     apps = {},
     supervisor = [[
+
+[program:prometheus]
+command=/bin/bash _SITE_ROOT_/scripts/run loop _service_prometheus _SITE_ROOT_
+directory=_SITE_ROOT_/data/prometheus
+redirect_stderr=true         
+stdout_logfile=_SITE_ROOT_/logs/prometheus.log
+autorestart=true
+
+
+[program:grafana]
+command=/bin/bash _SITE_ROOT_/scripts/run loop _service_grafana _SITE_ROOT_
+directory=_SITE_ROOT_/data/grafana
+redirect_stderr=true         
+stdout_logfile=_SITE_ROOT_/logs/grafana.log
+autorestart=true
     ]]
 }
 return _config
