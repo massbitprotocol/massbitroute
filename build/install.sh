e0c35e5c9f1251fc17bfdd3168b892a47e727472
Vu Tran <baysao@gmail.com>
2021-10-21 21:12:33 +0000

    add script install openresty

--- build/install.sh
+++ build/install.sh
@@ -1,4 +1,5 @@
-apt-get -y install --no-install-recommends wget gnupg ca-certificates
+apt-get -y install --no-install-recommends wget gnupg ca-certificates \
+build-essential libpcre3-dev libssl-dev git-core unzip python make zip unzip
 wget -O - https://openresty.org/package/pubkey.gpg | sudo apt-key add -
 echo "deb http://openresty.org/package/ubuntu $(lsb_release -sc) main" \
 	    | sudo tee /etc/apt/sources.list.d/openresty.list
