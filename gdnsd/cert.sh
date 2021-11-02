19126e0955ae41e345d36a8e1f544b167b1a603a
Vu Tran <baysao@gmail.com>
2021-11-02 08:57:38 +0700

    remove gdnsd

--- gdnsd/cert.sh
+++
@@ -1,8 +0,0 @@
-#apt-add-repository ppa:certbot/certbot
-#apt install certbot
-#wget https://github.com/joohoi/acme-dns-certbot-joohoi/raw/master/acme-dns-auth.py
-##!/usr/bin/env python3
-#chmod +x acme-dns-auth.py
-#mv acme-dns-auth.py /etc/letsencrypt/
-#certbot certonly --manual --manual-auth-hook /etc/letsencrypt/acme-dns-auth.py --preferred-challenges dns --debug-challenges -d \*.admoncdn.com -d admoncdn.com
-certbot renew
