5b182ad78c567c0a79cbd892ec04355b6132f60e
Vu Tran <baysao@gmail.com>
2021-12-14 14:23:04 +0700

    Remove config after update

--- app/src/sites/portal/apps/api/jobs/ApiAction.lua
+++ app/src/sites/portal/apps/api/jobs/ApiAction.lua
@@ -2,10 +2,11 @@ local cc = cc
 local gbc = cc.import("#gbc")
 local json = cc.import("#json")
 local cjson = require("cjson")
+local lfs = require("lfs")
 local io_open = io.open
 local mytype = "api"
 local JobsAction = cc.class(mytype .. "JobsAction", gbc.ActionBase)
-
+local mkdirp = require "mkdirp"
 local inspect = require "inspect"
 
 JobsAction.ACCEPTED_REQUEST_TYPE = "worker"
@@ -16,8 +17,44 @@ local CodeGen = require "CodeGen"
 
 local _deploy_dir = "/massbit/massbitroute/app/src/sites/portal/public/deploy/dapi"
 
+local function _read_file(path)
+    local file = io_open(path, "rb") -- r read mode and b binary mode
+    if not file then
+        return nil
+    end
+    local content = file:read "*a" -- *a or *all reads the whole file
+    file:close()
+    return content
+end
+
+local function _show_folder(folder)
+    local _files = {}
+    setmetatable(_files, cjson.array_mt)
+    mkdirp(folder)
+    for _file in lfs.dir(folder) do
+        if _file ~= "." and _file ~= ".." then
+            _files[#_files + 1] = _file
+        end
+    end
+    return _files
+end
+
+local function _read_dir(folder)
+    print("read_dir")
+    local _files = _show_folder(folder)
+    -- print(inspect(_files))
+    local _content = {}
+    for _, _file in ipairs(_files) do
+        print(folder .. "/" .. _file)
+        local _cont = _read_file(folder .. "/" .. _file)
+        -- print(_cont)
+        table.insert(_content, _cont)
+    end
+    return table.concat(_content, "\n")
+end
+
 -- local CodeGen = require "CodeGen"
-local mkdirp = require "mkdirp"
+
 -- local gwman_dir = "/massbit/massbitroute/app/src/sites/services/gwman"
 -- local stat_dir = "/massbit/massbitroute/app/src/sites/services/stat"
 
@@ -182,6 +219,7 @@ local function dirname(str)
 end
 
 local function _norm(_v)
+    print(inspect(_v))
     if type(_v) == "string" then
         _v = json.decode(_v)
     end
@@ -196,7 +234,7 @@ local function _norm(_v)
     return _v
 end
 
-local function _git_push(_dir, _files)
+local function _git_push(_dir, _files, _rfiles)
     local _git = "git -C " .. _dir .. " "
     local _cmd =
         "export HOME=/tmp && " ..
@@ -206,11 +244,18 @@ local function _git_push(_dir, _files)
                     "config --global user.email baysao@gmail.com" ..
                         "&&" .. _git .. "config --global user.name baysao && " .. _git .. "remote -v"
     for _, _file in ipairs(_files) do
-        mkdirp(dirname(_file))
+        -- mkdirp(dirname(_file))
         _cmd = _cmd .. ";" .. _git .. "add -f " .. _file
     end
+    if _rfiles then
+        for _, _file in ipairs(_rfiles) do
+            -- mkdirp(dirname(_file))
+            _cmd = _cmd .. ";" .. _git .. "rm -f " .. _file
+        end
+    end
+
     _cmd = _cmd .. " ; " .. _git .. "commit -m update && " .. _git .. "push origin master"
-    -- print(_cmd)
+    print(_cmd)
     local retcode, output = os.capture(_cmd)
     print(retcode)
     print(output)
@@ -234,6 +279,34 @@ local function _get_tmpl(_rules, _data)
     local _rules1 = table.copy(_rules)
     table.merge(_rules1, _data)
     return CodeGen(_rules1)
+end
+
+local function _remove_item(instance, args)
+    local model = Model:new(instance)
+    local _item = _norm(model:get(args))
+
+    model:delete(args)
+    -- _item = _item and type(_item) == "string" and json.decode(_item)
+    print(inspect(_item))
+
+    local _k1 = _item.blockchain .. "-" .. _item.network
+    local _deploy_dir1 = _deploy_dir .. "/nodes/" .. _k1
+    mkdirp(_deploy_dir1)
+    local _deploy_file = _deploy_dir1 .. "/" .. _item.id .. ".conf"
+    os.remove(_deploy_file)
+
+    local _content_all = _read_dir(_deploy_dir1)
+    local _content_all_file = _deploy_dir .. "/" .. _k1 .. ".conf"
+    _write_file(_content_all_file, _content_all)
+    _git_push(
+        _deploy_dir,
+        {
+            _content_all_file
+        },
+        {
+            _deploy_file
+        }
+    )
 end
 
 local function _generate_item(instance, args)
@@ -286,13 +359,19 @@ local function _generate_item(instance, args)
     -- This folder also public for Gateway Community download for serving blockchain-network traffic
 
     local _k1 = _item.blockchain .. "-" .. _item.network
-    local _deploy_file = _deploy_dir .. "/" .. _k1 .. ".conf"
+    local _deploy_dir1 = _deploy_dir .. "/nodes/" .. _k1
+    mkdirp(_deploy_dir1)
+    local _deploy_file = _deploy_dir1 .. "/" .. _item.id .. ".conf"
     _write_file(_deploy_file, table.concat(_content, "\n"))
 
+    local _content_all = _read_dir(_deploy_dir1)
+    local _content_all_file = _deploy_dir .. "/" .. _k1 .. ".conf"
+    _write_file(_content_all_file, _content_all)
     _git_push(
         _deploy_dir,
         {
-            _deploy_file
+            _deploy_file,
+            _content_all_file
         }
     )
 end
@@ -305,6 +384,14 @@ function JobsAction:generateconfAction(job)
     local instance = self:getInstance()
     local job_data = job.data
     _generate_item(instance, job_data)
-    -- _update_gateway_conf(instance)
 end
+
+function JobsAction:removeconfAction(job)
+    print(inspect(job))
+
+    local instance = self:getInstance()
+    local job_data = job.data
+    _remove_item(instance, job_data)
+end
+
 return JobsAction
