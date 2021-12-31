bb1cefede3c2f8fede55c339a268cafc5f8c4873
Vu Tran <baysao@gmail.com>
2021-12-31 11:50:58 +0700

    add confirm login with sending email

--- app/src/sites/portal/apps/api/actions/UserAction.lua
+++ app/src/sites/portal/apps/api/actions/UserAction.lua
@@ -1,21 +1,44 @@
 local cc, ngx = cc, ngx
 local gbc = cc.import("#gbc")
-local type = "User"
+local mytype = "user"
 local Session = cc.import("#session")
 
 local json = cc.import("#json")
 
-local Action = cc.class(type .. "Action", gbc.ActionBase)
+local Action = cc.class(mytype .. "Action", gbc.ActionBase)
 
 local _opensession
 
-local User = cc.import("#user")
+local set_var = ndk.set_var
 
+local Model = cc.import("#" .. mytype)
+
+-- local User = cc.import("#user")
+local util = cc.import("#mbrutil")
+local inspect = require "inspect"
+
+local _print = util.print
 local _user
 
 local ERROR = {
     NOT_LOGIN = 100
 }
+
+local function _norm_json(_v, _field)
+    if _v[_field] and type(_v[_field]) == "string" then
+        _v[_field] = json.decode(_v[_field])
+        setmetatable(_v[_field], cjson.empty_array_mt)
+    end
+end
+
+local function _norm(_v)
+    if type(_v) == "string" then
+        _v = json.decode(_v)
+    end
+    -- _norm_json(_v, "geo")
+    return _v
+end
+
 -- local inspect = require "inspect"
 
 function Action:pingAction(args)
@@ -27,9 +50,55 @@ function Action:pingAction(args)
         return {result = false, err_code = ERROR.NOT_LOGIN}
     end
 
+    instance:getRedis():setKeepAlive()
     return {
         result = true
     }
+end
+
+function Action:registerconfirmAction(args)
+    local _token = args.token
+    if not _token then
+        return {result = false}
+    end
+    local instance = self:getInstance()
+    local model = Model:new(instance)
+
+    local _data
+    if not args.baysao then
+        local token = set_var.set_decode_base32(_token)
+        _data = set_var.set_decrypt_session(token)
+        if _data then
+            _data = json.decode(_data)
+            if not _data.id or not _data.confirmtype or _data.confirmtype ~= "register" then
+                return ngx.redirect("https://dapi.massbit.io/pages/register_confirmed_fail.html")
+            end
+        else
+            return ngx.redirect("https://dapi.massbit.io/pages/register_confirmed_fail.html")
+        end
+
+        _print(inspect(_data))
+    else
+        local _username = args.baysao
+        local _id = model:get("name", _username)
+        _data = {id = _id}
+    end
+
+    local _is_confirmed = model:get(_data.id, "confirmed")
+    _print("is_confirmed:" .. inspect(_is_confirmed))
+    if _is_confirmed then
+        return ngx.redirect("https://dapi.massbit.io/pages/register_confirmed_fail.html")
+    -- return {result = false, err = "Account confirmed"}
+    end
+
+    _data.confirmed = true
+    model:update(_data)
+
+    ngx.redirect("https://dapi.massbit.io/pages/register_confirmed_success.html")
+    -- return {
+    --     result = true
+    -- }
+    -- local instance = self:getInstance()
 end
 
 function Action:registerAction(args)
@@ -38,14 +107,115 @@ function Action:registerAction(args)
 
     local instance = self:getInstance()
 
-    _user = _user or User:new(instance)
+    _user = _user or Model:new(instance)
 
+    args.confirmed = false
     local _detail, _err = _user:register(args)
     if not _detail then
         return {result = false, err = _err}
     end
 
+    -- local _data = {id = _detail.id}
+    -- local _txt = json.encode(_data)
+
+    -- _print("txt:" .. inspect(_txt))
+    local __data = set_var.set_encrypt_session(json.encode({id = _detail.id, confirmtype = "register"}))
+    local _token = set_var.set_encode_base32(__data)
+
+    _detail.token = _token
+    -- _print("detail:" .. inspect(_detail))
+    local jobs = instance:getJobs()
+    local job = {
+        action = "/jobs/" .. mytype .. ".register",
+        delay = 1,
+        data = _detail
+    }
+
+    local _ok, _err = jobs:add(job)
+
+    instance:getRedis():setKeepAlive()
     return {result = true, data = {id = _detail.id}}
+end
+
+-- function Action:changepassAction(args)
+--     args.action = nil
+--     -- ngx.log(ngx.ERR, json.encode(args))
+
+--     local instance = self:getInstance()
+
+--     _user = _user or Model:new(instance)
+
+--     args.confirmed = false
+--     local _detail, _err = _user:register(args)
+--     if not _detail then
+--         return {result = false, err = _err}
+--     end
+
+--     -- local _data = {id = _detail.id}
+--     -- local _txt = json.encode(_data)
+
+--     -- _print("txt:" .. inspect(_txt))
+--     local __data = set_var.set_encrypt_session(json.encode({id = _detail.id, confirmtype = "register"}))
+--     local _token = set_var.set_encode_base32(__data)
+
+--     _detail.token = _token
+--     -- _print("detail:" .. inspect(_detail))
+--     local jobs = instance:getJobs()
+--     local job = {
+--         action = "/jobs/" .. mytype .. ".register",
+--         delay = 1,
+--         data = _detail
+--     }
+
+--     local _ok, _err = jobs:add(job)
+
+--     return {result = true, data = {id = _detail.id}}
+-- end
+
+function Action:getAction(args)
+    _print(inspect(args))
+    -- if not args.id then
+    --     return {
+    --         result = false,
+    --         err_msg = "params 'id' missing"
+    --     }
+    -- end
+    args.action = nil
+    local instance = self:getInstance()
+    local _session = _opensession(instance, args)
+    -- _print("session:" .. inspect(_session))
+    if not _session then
+        return {result = false, err_code = ERROR.NOT_LOGIN}
+    end
+
+    local user_id = _session:get("id")
+    -- _print("user_id:" .. user_id)
+    -- if user_id then
+    --     args.user_id = user_id
+    --     args.id = user_id
+    -- end
+
+    local model = Model:new(instance)
+
+    -- _print("args:" .. inspect(args))
+    local _v, _err_msg = model:get(user_id)
+    _print("user:" .. inspect(_v))
+    _print("err:" .. inspect(_err_msg))
+    if _v then
+        _v = _norm(_v)
+        _v.password_hash = nil
+        _v.password_salt = nil
+
+        return {
+            result = true,
+            data = _v
+        }
+    end
+    instance:getRedis():setKeepAlive()
+    return {
+        result = false,
+        err_msg = _err_msg
+    }
 end
 
 function Action:loginAction(args)
@@ -61,8 +231,9 @@ function Action:loginAction(args)
     local appConfig = config.app
     -- ngx.log(ngx.ERR, json.encode(appConfig))
 
-    _user = _user or User:new(instance)
+    _user = _user or Model:new(instance)
     local _detail, _err = _user:login(args)
+    _print(inspect(_detail))
     if not _detail then
         return {result = false, err = _err}
     end
@@ -83,11 +254,29 @@ function Action:loginAction(args)
                     ngx.var.http_host .. ";Path=/;Expires=" .. _new_ttl .. ";Max-Age=" .. _new_ttl .. ";HttpOnly"
     }
 
+    _detail.site_root = ngx.var.site_root
+    _detail.ip = ngx.var.realip
+    _detail.user_agent = ngx.var.http_user_agent
+    _detail.time = os.date("!%Y-%m-%d %H:%M:%S")
+
+    --os.time(os.date("!*t"))
+
+    --ngx and ngx.time() or os.time()
+    local jobs = instance:getJobs()
+    local job = {
+        action = "/jobs/" .. mytype .. ".login",
+        delay = 1,
+        data = _detail
+    }
+
+    local _ok, _err = jobs:add(job)
+    instance:getRedis():setKeepAlive()
     -- return result
     return {result = true, data = {sid = _sid}}
 end
 
 function Action:logoutAction(args)
+    local instance = self:getInstance()
     args.action = nil
     -- remove user from online list
     local session = _opensession(self:getInstance(), args)
@@ -98,6 +287,7 @@ function Action:logoutAction(args)
     -- online:remove(session:get("username"))
     -- delete session
     session:destroy()
+    instance:getRedis():setKeepAlive()
     return {ok = "ok"}
 end
 
