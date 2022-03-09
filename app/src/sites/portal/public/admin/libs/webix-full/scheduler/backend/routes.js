2a6111dc8ddb834e042ef62e01e1e1a4665cbccf
Vu Tran <baysao@gmail.com>
2022-03-09 17:04:58 +0700

    clean up files

--- app/src/sites/portal/public/admin/libs/webix-full/scheduler/backend/routes.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-	const root = "/scheduler/samples/server";
-	require("./data")(app, root);
-	require("./dyn_data")(app, root);
-}
--- apps/api/conf/app_config.lua
+++ apps/api/conf/app_config.lua
@@ -1,28 +1,9 @@
 local _config = {
-    whitelist_sid = {
-        ["403716b0f58a7d6ddec769f8ca6008f2c1c0cea6"] = {
-            partner_id = "fc78b64c5c33f3f270700b0c4d3e7998188035ab"
-        }
-    },
-    whitelist_test = {
-        ["massbit"] = 1,
-        ["duongvu"] = 1,
-        ["keith@anorak.technology"] = 1,
-        ["cryptochainnakoturk@gmail.com"] = 1,
-        ["da686rk@gmail.com"] = 1,
-        ["dalala2022@outlook.com"] = 1,
-        ["aalgesis@gmail.com"] = 1,
-        ["luka@pops.one"] = 1,
-        ["cryptos.airdrops07@gmail.com"] = 1,
-        ["manojmaharjan1112@gmail.com"] = 1,
-        ["ndmt2110@gmail.com"] = 1,
-        ["semkbssa@gmail.com"] = 1,
-        ["mandarpharande98@gmail.com"] = 1,
-        ["sherrychauha42@gmail.com"] = 1
-    },
+    whitelist_sid = {},
+    whitelist_test = {},
     sessionExpiredTime = 60 * 30,
     numOfJobWorkers = 10,
-    sengrid_key = "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",
-    ipapi_token = "e660b739310497215d77e593f4bfe1bc"
+    sengrid_key = "",
+    ipapi_token = ""
 }
 return _config
