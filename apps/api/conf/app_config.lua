8becfb5b382ede82b9ad16e3c2d88d3976e1a9ab
Viet Tai <vuviettai@gmail.com>
2022-03-23 13:13:17 +0700

    Using env variable in app_config.lua

--- apps/api/conf/app_config.lua
+++ apps/api/conf/app_config.lua
@@ -1,9 +1,15 @@
+local env = require("env")
 local _config = {
-    whitelist_sid = {},
+    portal_domain = env.MBR_API,
+    whitelist_sid = {
+        [env.SID] = {
+            partner_id = env.PARTNER_ID
+        }
+    },
     whitelist_test = {},
     sessionExpiredTime = 60 * 30,
     numOfJobWorkers = 10,
-    sengrid_key = "",
-    ipapi_token = ""
+    sengrid_key = env.SENDGRID_KEY,
+    ipapi_token = env.IPAPI_TOKEN
 }
 return _config
