14e809354158e9d6ba968092e726b61bfd00f774
Vu Tran <baysao@gmail.com>
2022-05-23 16:09:43 +0700

    Debug gwman generate conf

--- apps/api/jobs/GatewayAction.lua
+++ apps/api/jobs/GatewayAction.lua
@@ -202,6 +202,7 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
                                 _dc_block[_continent] = _dc_block[_continent] or {}
                                 _dc_block1[_continent] = _dc_block1[_continent] or {}
                                 _dc_block[_continent][_country] = _dc_block[_continent][_country] or {}
+                                _dc_block1[_continent][_country] = _dc_block1[_continent][_country] or {}
                                 _dc_block[_continent]["default"] = _dc_block[_continent]["default"] or {}
                                 _dc_block1[_continent]["default"] = _dc_block1[_continent]["default"] or {}
                                 _dc_block["default"] = _dc_block["default"] or {}
