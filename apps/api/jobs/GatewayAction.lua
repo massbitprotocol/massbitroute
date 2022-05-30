8e31329d294d9271caac261129ef1c8cc3e68ba0
Vu Tran <baysao@gmail.com>
2022-05-30 12:04:40 +0700

    Remove dead code

--- apps/api/jobs/GatewayAction.lua
+++ apps/api/jobs/GatewayAction.lua
@@ -7,25 +7,19 @@ local JobsAction = cc.class(mytype .. "JobsAction", gbc.ActionBase)
 
 local mbrutil = require "mbutil" --cc.import("#mbrutil")
 local env = require("env")
--- local read_dir = mbrutil.read_dir
+
 local read_file = mbrutil.read_file
 local show_folder = mbrutil.show_folder
 local inspect = mbrutil.inspect
 
-local shell = require "shell-games"
-
--- local table_map = table.map
--- local table_walk = table.walk
--- local table_filter = table.filter
 local table_insert = table.insert
 local table_concat = table.concat
 local table_keys = table.keys
--- local table_keys = table.keys
+
 local table_merge = table.merge
 
 local _write_file = mbrutil.write_file
 local _get_tmpl = mbrutil.get_template
--- local _git_push = mbrutil.git_push
 
 local mkdirp = require "mkdirp"
 
@@ -143,13 +137,11 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
     local _dc_geo = {}
 
     local _network_dir = _deploy_dir .. "/" .. _blockchain .. "/" .. _network
-    -- _print("dir:" .. _network_dir)
+
     for _, _continent in ipairs(show_folder(_network_dir)) do
         local _continent_dir = _network_dir .. "/" .. _continent
         for _, _country in ipairs(show_folder(_continent_dir)) do
             local _geo_id = _blocknet_id .. "-" .. _continent .. "-" .. _country
-            -- local _geo_country_default = _blocknet_id .. "-" .. _continent .. "-default"
-            -- local _geo_continent_default = _blocknet_id .. "-default-default"
 
             local _country_dir = _continent_dir .. "/" .. _country
             for _, _user_id in ipairs(show_folder(_country_dir)) do
@@ -163,16 +155,13 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
                             _item = json.decode(_item)
                         end
                         _item._domain_name = _job_data._domain_name
-                        -- local _ip = _item.ip
-                        -- print("ip:" .. inspect(_ip))
+
                         local _obj = {
                             ip = _item.ip,
                             id = _item.id,
-                            -- id_block = _item.blockchain .. "-" .. _item.network,
                             geo_id = _geo_id
-                            --_blocknet_id .. "-" .. _item.geo.continent_code .. "-" .. _item.geo.country_code
                         }
-                        -- _print({id = _item.id, status = _item.status}, true)
+
                         if _continent and _country and _item.status and _item.approved then
                             local _t =
                                 _blocknet_id ..
@@ -188,10 +177,6 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
                             table.insert(_allnodes[_t2], _item)
                         end
 
-                        -- if _item.status and tonumber(_item.status) == 0 then
-                        --     _not_actives[#_not_actives + 1] = _item
-                        -- end
-
                         if _item.status and tonumber(_item.status) == 1 then
                             _item._is_enabled = true
                             _actives[#_actives + 1] = _item
@@ -201,9 +186,6 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
 
                                 _datacenters["geo"] = _datacenters["geo"] or {}
                                 _datacenters["blocknet"] = _datacenters["blocknet"] or {}
-                                -- local _dc_geo = _datacenters["geo"]
-                                -- local _dc_block = _datacenters["blocknet"]
-                                -- _datacenters["blocknet1"] = _datacenters["blocknet1"] or {}
 
                                 _dc_global[_geo_id] = 1
 
@@ -216,28 +198,9 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
 
                                 _dc_country[_continent][_country][_geo_id] = 1
 
-                                -- _dc_geo[_blocknet_id] = _dc_geo[_blocknet_id] or {}
-                                -- _dc_block[_blocknet_id] = _dc_block[_blocknet_id] or {}
-                                -- _dc_block[_continent] = _dc_block[_continent] or {}
-
-                                -- _dc_block[_continent][_country] = _dc_block[_continent][_country] or {}
-
-                                -- _dc_block[_continent]["default"] = _dc_block[_continent]["default"] or {}
-
-                                -- _dc_block["default"] = _dc_block["default"] or {}
-                                -- -- _dc_block1["default"] = _dc_block1["default"] or {}
-
-                                -- _dc_block[_continent][_country] = _geo_id
-
-                                -- table.insert(_dc_block[_continent]["default"], _geo_id)
-                                -- table.insert(_dc_block["default"], _geo_id)
-
                                 _dc_geo[_geo_id] = _dc_geo[_geo_id] or {}
-                                -- _dc_geo[_geo_country_default] = _dc_geo[_geo_country_default] or {}
-                                -- _dc_geo[_geo_continent_default] = _dc_geo[_geo_continent_default] or {}
+
                                 table_insert(_dc_geo[_geo_id], _obj)
-                            -- table_insert(_dc_geo[_geo_country_default], _obj)
-                            -- table_insert(_dc_geo[_geo_continent_default], _obj)
                             end
                         end
                     end
@@ -327,88 +290,12 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
         print(_file_map)
         _write_file(_file_map, _geo_map)
     end
-    -- if _datacenters["blocknet"] and next(_datacenters["blocknet"]) ~= nil then
-    --     local _geo_val = _datacenters["blocknet"]
-    --     _print("blocknet:")
-    --     _print(_geo_val, true)
 
-    --     local _v_maps = {}
-    --     local _v_datacenters = {}
-    --     table_insert(_v_maps, _blocknet_id .. " => { ")
-
-    --     for _k3, _v3 in pairs(_geo_val) do
-    --         if _k3 ~= "default" then
-    --             table_insert(_v_maps, _k3 .. " => { ")
-    --             for _k4, _v4 in pairs(_v3) do
-    --                 if _k4 and _k4 ~= "default" then
-    --                     table_insert(_v_maps, "  " .. _k4 .. " => [ " .. _v4 .. "],")
-    --                     _v_datacenters[_v4] = 1
-    --                 else
-    --                     _v_datacenters[_blocknet_id .. "-" .. _k3 .. "-" .. _k4] = 1
-    --                     -- if _v4 then
-    --                     --     table.insert(_v_datacenters, _v4)
-    --                     -- end
-    --                     local _cache = {}
-    --                     table_insert(_v_maps, "  " .. _k4 .. " => [ ")
-    --                     for _, _v5 in ipairs(_v4) do
-    --                         if _v5 and not _cache[_v5] then
-    --                             table_insert(_v_maps, "    " .. _v5 .. ",")
-    --                             _cache[_v5] = 1
-    --                             _v_datacenters[_v5] = 1
-    --                         end
-    --                     end
-    --                     table_insert(_v_maps, "  ],")
-    --                 end
-    --             end
-    --             table_insert(_v_maps, "},")
-    --         else
-    --             _v_datacenters[_blocknet_id .. "-" .. _k3 .. "-" .. _k3] = 1
-    --             table_insert(_v_maps, "  " .. _k3 .. " => [ ")
-    --             local _cache = {}
-    --             for _, _v4 in ipairs(_v3) do
-    --                 if _v4 and not _cache[_v4] then
-    --                     table_insert(_v_maps, "    " .. _v4 .. ",")
-    --                     _v_datacenters[_v4] = 1
-    --                     _cache[_v4] = 1
-    --                 end
-    --             end
-    --             table_insert(_v_maps, "  ],")
-    --         end
-    --     end
-
-    --     table_insert(_v_maps, "}")
-    --     _print("v_maps:" .. inspect(_v_maps))
-    --     _print("v_datacenters:" .. inspect(_v_datacenters))
-    --     local _tmpl_map =
-    --         _get_tmpl(
-    --         rules,
-    --         {
-    --             id = _blocknet_id,
-    --             datacenters = table_keys(_v_datacenters),
-    --             map = table_concat(_v_maps, "\n"),
-    --             _domain_name = _job_data._domain_name
-    --         }
-    --     )
-    --     local _geo_map = _tmpl_map("_dns_geo_map")
-    --     print(_geo_map)
-    --     local _file_map = gwman_dir .. "/conf.d/geolocation.d/maps.d/mbr-map-" .. _blocknet_id
-    --     print(_file_map)
-    --     _write_file(_file_map, _geo_map)
-    -- end
-
-    -- if _datacenters["geo"] and next(_datacenters["geo"]) ~= nil then
     _print("dc_geo")
     _print(_dc_geo, true)
     if _dc_geo and next(_dc_geo) then
-        -- local _geo_val = _datacenters["geo"]
-        -- _print("geo:")
-        -- _print(_geo_val, true)
-
-        -- _print("_geo_val:" .. inspect(_geo_val))
         local _dc_maps_new = {}
         for _geo_id, _geo_svrs in pairs(_dc_geo) do
-            -- _print("_geo_id:" .. inspect(_geo_id))
-            -- _print("_geo_svrs:" .. inspect(_geo_svrs))
             table.insert(
                 _dc_maps_new,
                 {
@@ -417,8 +304,6 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
                 }
             )
         end
-
-        -- _print("_dc_maps_new:" .. inspect(_dc_maps_new))
 
         local _tmpl_res =
             _get_tmpl(
@@ -445,8 +330,6 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
         local _str_stat = _tmpl("_gw_stat_v1")
         mkdirp(stat_dir .. "/stat_gw")
         local _file_stat = stat_dir .. "/stat_gw/" .. _blocknet_id .. ".yml"
-        -- _print(_str_stat)
-        -- _print(_file_stat)
         _write_file(_file_stat, _str_stat)
     end
 
@@ -462,15 +345,7 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
         local _str_listid = _tmpl("_listids")
         mkdirp(_info_dir .. "/" .. mytype)
         local _file_listid = _info_dir .. "/" .. mytype .. "/listid-" .. _blocknet_id
-        -- _print(_str_listid)
-        -- _print(_file_listid)
         _write_file(_file_listid, _str_listid)
-
-    -- local _str_listid_not_actives = _tmpl("_listids_not_actives")
-    -- local _file_listid_not_actives = _info_dir .. "/" .. mytype .. "/listid-not-active-" .. _blocknet_id
-    -- _print(_str_listid_not_actives)
-    -- _print(_file_listid_not_actives)
-    -- _write_file(_file_listid_not_actives, _str_listid_not_actives)
     end
     if _allnodes and next(_allnodes) then
         for _t, _v in pairs(_allnodes) do
@@ -478,8 +353,7 @@ local function _rescanconf_blockchain_network(_blockchain, _network, _job_data)
             local _str_listid = _tmpl("_listids")
             mkdirp(_info_dir .. "/" .. mytype)
             local _file_listid = _info_dir .. "/" .. mytype .. "/listid-" .. _t
-            -- _print(_str_listid)
-            -- _print(_file_listid)
+
             _write_file(_file_listid, _str_listid)
         end
     end
