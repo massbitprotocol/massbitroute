378ae10276bc38ecf589a00b30cdd902546b35e3
hoanito <hhoang219@gmail.com>
2022-06-17 11:31:59 +0700

    test ci

--- scripts/single-node-test/provider.tf
+++ scripts/single-node-test/provider.tf
@@ -1,6 +1,6 @@
 provider "google-beta" {
     credentials = file("./project_key.json")
     project = "mbr-dev-341307" //replace project name here
-    region  = "asia-southeast2"
-    zone    = "asia-southeast2-a"
+    region  = "asia-southeast1"
+    zone    = "asia-southeast1-b"
 }
