98abc4a41894e9c9b10a6ebfecc5f6c9fff6b279
Vu Tran <baysao@users.noreply.github.com>
2022-08-17 14:42:39 +0700

    Release (#213)
    * chore: sleep and check file exists #198
    
    * bugfix: check env lua existed #5
    
    * feat: add debug info for node register
    
    * chore: sleep and check file exists #198
    
    * chore: select keiko is default env
    
    * bugfix: branch conflict resolve
    
    * bugfix: check env lua existed #5
    
    * bugfix: branch conflict resolve
    
    * bugfix: branch conflict resolve
    
    * fix: supervisor option
    
    * fix: clean up flow
    
    * fix: cleanup update_repos
    
    * fix: cleanup update_repos
    
    * chore: add _env to update env
    
    * chore: refresh env every private repo update
    
    * fix: add _env function
    
    * fix: check env dir existence
    
    * fix: load _env in init func
    
    * chore: enable _update flow
    
    * chore: enable _update flow
    
    * chore: enable _update flow
    
    * chore: update update_repos, install_repos
    
    * chore: add commit_repos
    
    * chore: add commit_repos
    
    * fix: wrong path
    
    * fix: wrong path
    
    * fix: wrong path var name
    
    * fix: wrong status var
    
    * fix: wrong status var
    
    * fix: add load _env
    
    * chore: seperate private with runtime repos
    
    * fix: clone only dir without .git dir
    
    * fix: do not check var GIT_PRIVATE_READ_URL in install repos
    
    * fix: add runtime_repos in update repos phase
    
    * fix: change format default for env whitelist_partners
    
    * chore: change allow ip for internal api via env API_INTERNAL_IP_ALLOW
    
    * fix: update HOME & remove check  GIT_PRIVATE_READ_URL in install phase
    
    * fix: update HOME & remove check  GIT_PRIVATE_READ_URL in install phase
    
    * fix: do not call _env on _repo
    
    * fix: do not call _env on _repo
    
    * fix: do not call _env on _repo
    
    * fix: clean up GIT_PUBLIC_URL
    
    * fix: clean up GIT_PUBLIC_URL
    
    * fix: clean up GIT_PUBLIC_URL
    
    * fix: clean up GIT_PUBLIC_URL
    
    * chore: add API_DOMAIN_SSL env
    
    * chore: replace exit by return
    
    * chore: add _update
    
    * fix: update _reload
    
    * fix: add define DOMAIN or auto extract from PORTAL_URL
    
    * chore: using GIT_PRIVATE_BRANCH as private branch for all private repo
    
    * chore: turn autostart on (#212)

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 SITE_ROOT=$(realpath $(dirname $(realpath $0))/..)
-
+export HOME=$SITE_ROOT
 _git_config() {
-	# if [ ! -f "$HOME/.gitconfig" ]; then
 	cat >$HOME/.gitconfig <<EOF
    [http]
         sslverify = false
@@ -10,84 +9,127 @@ _git_config() {
 	email = baysao@gmail.com
 	name = Baysao
 EOF
-	# fi
+}
+_env() {
+	if [ -z "$MBR_ENV" ]; then
+		echo "MBR_ENV missing"
+		return 1
+	fi
+	if [ -z "$GIT_PRIVATE_READ_URL" ]; then
+		echo "GIT_PRIVATE_READ_URL missing"
+		return 1
+	fi
+	export ENV_BRANCH=${ENV_BRANCH:-$MBR_ENV}
+	if [ ! -d "$SITE_ROOT/env/.git" ]; then
+		_git_clone $GIT_PRIVATE_READ_URL/massbitroute/env.git $SITE_ROOT/env $ENV_BRANCH
+	fi
+	if [ -f "$SITE_ROOT/env/env.sh" ]; then
+		source $SITE_ROOT/env/env.sh
+	fi
+	if [ -f "$SITE_ROOT/env/env.lua" ]; then
+		cp $SITE_ROOT/env/env.lua $SITE_ROOT/src/
+	fi
 
 }
 _git_clone() {
 	_url=$1
 	_dir=$2
 	_branch=$3
+	_force=$4
+	if [ -z "$_force" ]; then _force=0; fi
 	if [ -z "$_branch" ]; then _branch=$MBR_ENV; fi
-	# if [ -d "$_dir" ]; then rm -rf $_dir; fi
-	mkdir -p $_dir
-	git config --global --add safe.directory $_dir
-	if [ ! -d "$_dir/.git" ]; then
-		git clone $_url $_dir -b $_branch
+	_clone_status=0
 
-		git -C $_dir fetch --all
-		git -C $_dir branch --set-upstream-to=origin/$_branch
-
-	else
-		git -C $_dir remote -v | grep 'git@' >/dev/null
-		if [ $? -ne 0 ]; then
-			git -C $_dir fetch --all
-			git -C $_dir pull origin $_branch
+	if [ $_force -eq 1 ]; then
+		if [ -d "$_dir" ]; then
+			if [ -d "${_dir}.backup" ]; then rm -rf ${_dir}.backup; fi
+			mv $_dir ${_dir}.backup
 		fi
 	fi
-	if [ -f "$_dir/scripts/run" ]; then
-		echo "========================="
-		echo "$_dir/scripts/run _prepare"
-		echo "========================="
-		$_dir/scripts/run _prepare
+
+	mkdir -p $_dir
+
+	if [ ! -d "$_dir/.git" ]; then
+		git clone --depth 1 -b $_branch $_url $_dir
+		_clone_status=1
+
 	fi
+
+	git -C $_dir pull origin $_branch | grep -i "updating" >/dev/null
+	if [ $? -eq 0 ]; then
+		_clone_status=1
+	fi
+
+	return $_clone_status
 
 }
 
-_update_sources() {
+_install_sources() {
 	_git_config
-	_is_reload=0
-	branch=$MBR_ENV
+	# _env
+	_install_sources_status=0
 	for _pathgit in $@; do
-		_path=$(echo $_pathgit | cut -d'|' -f1)
-		git config --global --add safe.directory $_path
+		# _repo
+		# _env
+		_dir=$(echo $_pathgit | cut -d'|' -f1)
 		_url=$(echo $_pathgit | cut -d'|' -f2)
 		_branch=$(echo $_pathgit | cut -d'|' -f3)
-		if [ -z "$_branch" ]; then _branch=$branch; fi
-		if [ ! -d "$_path/.git" ]; then
 
-			git clone $_url $_path -b $_branch
-			git -C $_path fetch --all
-			git -C $_path branch --set-upstream-to=origin/$_branch
-			_is_reload=1
-		else
+		if [ -z "$_branch" ]; then _branch=$MBR_ENV; fi
+		_git_clone $_url $_dir $_branch 1
+		_st=$?
 
-			git -C $_path remote -v | grep $_url >/dev/null
-			if [ $? -ne 0 ]; then
-				rm -rf $_path
-				git clone $_url $_path -b $_branch
-			fi
 
-			git -C $_path fetch --all
-			git -C $_path checkout $_branch
-
-			git -C $_path branch | grep $_branch >/dev/null
-			if [ $? -ne 0 ]; then
-				git -C $_path reset --hard
-			fi
-
-			tmp="$(git -C $_path pull origin $_branch 2>&1)"
-
-			echo "$tmp" | grep -i "updating"
-			st=$?
-			echo $_path $st
-			if [ $st -eq 0 ]; then
-				_is_reload=1
-			fi
-
+		if [ $_install_sources_status -eq 0 ]; then
+			_install_sources_status=$_st
 		fi
-
+		_env
 	done
-	return $_is_reload
+	return $_install_sources_status
+}
+_update_sources() {
+	_git_config
+	_env
+	_update_sources_status=0
+	for _pathgit in $@; do
+		_dir=$(echo $_pathgit | cut -d'|' -f1)
+		# git config --global --add safe.directory $_path
+		_url=$(echo $_pathgit | cut -d'|' -f2)
+		_branch=$(echo $_pathgit | cut -d'|' -f3)
+		if [ -z "$_branch" ]; then _branch=$MBR_ENV; fi
+		_git_clone $_url $_dir $_branch
+		_st=$?
+
+		if [ $_update_sources_status -eq 0 ]; then
+			_update_sources_status=$_st
+		fi
+		_env
+	done
+	return $_update_sources_status
+}
+_commit_sources() {
+	_git_config
+	_env
+	_commit_sources_status=0
+	for _pathgit in $@; do
+		_dir=$(echo $_pathgit | cut -d'|' -f1)
+		# git config --global --add safe.directory $_path
+		_url=$(echo $_pathgit | cut -d'|' -f2)
+		_branch=$(echo $_pathgit | cut -d'|' -f3)
+		if [ -z "$_branch" ]; then _branch=$MBR_ENV; fi
+		cd $_dir
+		git pull origin $_branch
+		git add .
+		git commit -m "$(date)"
+		git push origin $_branch
+		_st=$?
+
+		if [ $_commit_sources_status -eq 0 ]; then
+			_commit_sources_status=$_st
+		fi
+		_env
+	done
+	return $_commit_sources_status
 }
 loop() {
 	while true; do
